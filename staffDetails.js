436df314260ebc394f01491909f0137efcd6a7b6
azimazmi98 <45591449+azimazmi98@users.noreply.github.com>
2020-06-17 01:07:51 +0800

    Add files via upload

---
+++ staffDetails.js
@@ -0,0 +1,70 @@
+const readline = require('readline');
+const { isNull } = require('util');
+
+
+const rl = readline.createInterface({
+
+input: process.stdin,
+
+output: process.stdout
+
+});
+
+
+// loop for 22 slots
+for(i=0; i<2; i++){
+
+// declare multiple arrays for each slot
+var staffName = [];
+var staffID = [];
+var staffStart = [];
+var staffEnd = [];
+
+// if the slot is empty it can proceed
+if(staffName[i] = isNull){
+
+// prompt for staff details
+rl.question('Enter your name? ', (name) => {
+    rl.question('Enter your StaffID? ', (id) => {
+        rl.question('Enter time when your shift starts: ', (startWH) => {
+            rl.question('Enter time when your shift ends: ', (endWH) => {
+            
+                staffName[i] = name;
+                staffID[i] = id;
+                staffStart[i] = startWH;
+                staffEnd[i] = endWH;
+
+                console.log(staffName[i]);
+                console.log(staffID[i]);
+                console.log(staffStart[i]);
+                console.log(staffEnd[i]);
+
+                rl.question('Recheck Y/N', (response) => {
+
+                    let userResponse = response.charAt(0);
+                    if(userResponse == 'Y' || 'y'){
+                        console.log(staffName[i]);
+                        console.log(staffID[i]);
+                        console.log(staffStart[i]);
+                        console.log(staffEnd[i]);
+                    }
+                    else
+                        return false;
+
+                    rl.question('Edit? Y/N', (edit) => {
+
+                            let userEdit = edit.charAt(0);
+                            if((userEdit == 'Y') || (userEdit == 'y')){
+                                console.log('Your details have been updated.')
+                            }
+                            else
+                    
+                    rl.close();
+                    });
+                    });
+                });
+            });
+        });
+    });
+}
+}
